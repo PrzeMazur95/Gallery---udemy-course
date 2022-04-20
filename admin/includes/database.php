0f2b92608206c9d9534a2bf0c995f52307c54f43
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-20 20:32:30 +0200

    Modify db class, add few functions

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -13,14 +13,43 @@ class Database {
 
     public function open_db_connection(){
 
-        $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-        if(mysqli_connect_errno()){
+        if($this->connection->connect_errno){
 
-            die("Database connection failed" . mysqli_error());
+            die("Database connection failed" . $this->connection->connect_error);
         }
 
     }
+
+    public function query($sql){
+
+        $sql = $this->escape_string($sql);
+
+        $result = $this->connection->query($sql);
+
+        $this->confirm_query($result);
+       
+            return $result;
+        
+    }
+
+    public function confirm_query($result){
+
+        if(!$result){
+
+            die("Query failed" . $this->connection->error);
+
+        }
+
+    }
+
+    public function escape_string($string){
+
+        $escaped_string = $this->connection->real_escape_string($string);
+        return $escaped_string;
+    }
+
 
 }
 
