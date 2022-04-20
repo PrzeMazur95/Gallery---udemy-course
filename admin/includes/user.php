9608f49a4c3bd2e5cbdd08a031db69fe2581caf3
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-20 21:53:34 +0200

    Improve user class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,23 +2,47 @@
 
 class User {
 
+
+    public $id;
+    public $username;
+    public $password;
+    public $first_name;
+    public $last_name;
+    
+
+
     public static function find_all_users() {
-        global $database;
-
-        $result_set = $database->query("SELECT * FROM users");
-        return $result_set;
-
+        
+        return self::find_this_query("SELECT * FROM users");
 
     }
 
     public static function find_by_id($id) {
-        global $database;
 
-        $result_set = $database->query("SELECT * FROM users WHERE id = {$id} LIMIT 1");
+        $result_set = self::find_this_query("SELECT * FROM users WHERE id = {$id} LIMIT 1");
         $found_user = mysqli_fetch_array($result_set);
         return $found_user;
 
+    }
 
+    public static function find_this_query($sql){
+        global $database;
+        $result_set = $database->query($sql);
+        return $result_set;
+
+    }
+
+    public static function instantation($user_found){
+
+        $the_object = new self;
+
+        $the_object->id=$user_found['id'];
+        $the_object->username=$user_found['username'];
+        $the_object->password=$user_found['password'];
+        $the_object->first_name=$user_found['first_name'];
+        $the_object->last_name=$user_found['last_name'];
+
+        return $the_object;
     }
 
 
