07008e726f966ba9880179b7e11e2fb416a62fe2
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-21 08:45:43 +0200

    More improves to User class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -17,32 +17,61 @@ class User {
 
     }
 
-    public static function find_by_id($id) {
+    public static function find_user_by_id($id) {
 
-        $result_set = self::find_this_query("SELECT * FROM users WHERE id = {$id} LIMIT 1");
-        $found_user = mysqli_fetch_array($result_set);
-        return $found_user;
+        global $database;
+
+        $the_result_array = self::find_this_query("SELECT * FROM users WHERE id = {$id}");
+
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
 
     }
 
     public static function find_this_query($sql){
         global $database;
         $result_set = $database->query($sql);
-        return $result_set;
+        $the_object_array = array();
+
+        while($row = mysqli_fetch_array($result_set)){
+            $the_object_array[] = self::instantation($row);
+        }
+
+        return $the_object_array;
 
     }
 
-    public static function instantation($user_found){
+    public static function instantation($the_record){
 
         $the_object = new self;
 
-        $the_object->id=$user_found['id'];
-        $the_object->username=$user_found['username'];
-        $the_object->password=$user_found['password'];
-        $the_object->first_name=$user_found['first_name'];
-        $the_object->last_name=$user_found['last_name'];
+        // $the_object->id=$user_found['id'];
+        // $the_object->username=$user_found['username'];
+        // $the_object->password=$user_found['password'];
+        // $the_object->first_name=$user_found['first_name'];
+        // $the_object->last_name=$user_found['last_name'];
 
+        //rozdziela otrzymaną tablicę (wyszuakny z DB) na key+value - username - karol
+        //wysyła key do następnej metody, która sprawza,czy dana klasa posada taki atrybut
+        //jeśeli posiada, przypisuje do tego atrybutu value
+
+        foreach ($the_record as $the_attribute => $value){
+
+            if($the_object->has_the_attribute($the_attribute)){
+
+                $the_object->$the_attribute=$value;
+
+            }
+        }
         return $the_object;
+    }
+
+        // sprawdza czy dana klasa posiada atrubut ze znalezionego wyniku z DB
+    private function has_the_attribute($the_attribute){
+
+        $object_properties = get_object_vars($this);
+
+        return array_key_exists($the_attribute, $object_properties);
+
     }
 
 
