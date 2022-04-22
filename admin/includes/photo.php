eb26b89070c734b36f6ac08c6b17cf7501b0b144
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-22 18:25:08 +0200

    Add upload errors array

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -12,6 +12,21 @@ class Photo extends Db_object {
     public $size;
 
 
+    public $tmp_path;
+    public $upload_directory = "images";
+    public $custom_errors = array();
+    public $upload_errors_array = array(
+
+        UPLOAD_ERR_OK          => "There is no error",
+        UPLOAD_ERR_INT_SIZE    => "File exceeds the upload_max_filesize",
+        UPLOAD_ERR_FORM_SIZE   => "File exceeds the max_file_size",
+        UPLOAD_ERR_PARTIAL     => "File was only partially uploaded",
+        UPLOAD_ERR_NO_FILE     => "No file was uploaded",
+        UPLOAD_ERR_NO_TMP_DIR  => "Missing temporary folder",
+        UPLOAD_ERR_CANT_WRITE  => "Failed to write file to disk",
+        UPLOAD_ERR_EXTENSION   => "A PHP extension stopped the file upload",
+
+    );
 
 }
 
