51062342a96bb6afe8c1b9c3bb1577370534ee3d
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-22 14:01:50 +0200

    Abstract user methods

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -3,6 +3,8 @@
 class User {
 
 
+    protected static $db_table = "users";
+    protected static $db_table_fields = array('nickname', 'pass', 'first_name', 'last_name');
     public $id;
     public $nickname;
     public $pass;
@@ -90,18 +92,57 @@ class User {
 
     }
 
+    protected function properties(){
+
+
+        $properties = array();
+        foreach (self::$db_table_fields as $db_field){
+
+            if(property_exists($this, $db_field)){
+
+                $properties[$db_field] = $this->$db_field;
+
+            }
+
+        }
+
+        return $properties;
+
+    }
+
+
+    protected function clean_properties(){
+
+        global $database;
+
+        $clean_properties = array();
+
+        foreach($this->properties() as $key => $value){
+
+            $clean_properties[$key] = $database->escape_string($value);
+        }
+
+        return $clean_properties;
+
+    }
+
+
+    public function save(){
+
+        return isset($this->id) ? $this->update() : $this->create();
+
+    }
 
     public function create (){
 
         global $database;
 
-        // $sql = "INSERT INTO users (nickname, pass, first_name, last_name) VALUES ('";
-        // $sql .= $database->escape_string($this->nickname) . "', '";
-        // $sql .= $database->escape_string($this->pass) . "', '";
-        // $sql .= $database->escape_string($this->first_name) . "', '";
-        // $sql .= $database->escape_string($this->last_name) . "')";
+        $properties = $this->clean_properties();
 
-        $sql = "INSERT INTO users SET nickname ='".$database->escape_string($this->nickname)."', pass='".$database->escape_string($this->pass)."', first_name='".$database->escape_string($this->first_name)."', last_name='".$database->escape_string($this->last_name)."'";
+        $sql = "INSERT INTO ".self::$db_table."(". implode(",", array_keys($properties)). ")"; 
+        $sql .="VALUES ('". implode("','", array_values($properties)) ."')";
+
+        // $sql = "INSERT INTO ".self::$db_table." SET nickname ='".$database->escape_string($this->nickname)."', pass='".$database->escape_string($this->pass)."', first_name='".$database->escape_string($this->first_name)."', last_name='".$database->escape_string($this->last_name)."'";
         
         
         
@@ -130,8 +171,22 @@ class User {
 
         global $database;
 
+        $properties = $this->clean_properties();
 
-        $sql = "UPDATE users SET nickname ='".$database->escape_string($this->nickname)."', pass='".$database->escape_string($this->pass)."', first_name='".$database->escape_string($this->first_name)."', last_name='".$database->escape_string($this->last_name)."' WHERE id=".$database->escape_string($this->id)."";
+        $properties_pairs = array();
+
+        foreach ($properties as $key => $value){
+
+            $properties_pairs[] = "{$key}='{$value}'";
+
+        }
+
+        $sql = "UPDATE " .self::$db_table." SET ";
+        $sql .= implode(", ", $properties_pairs);
+        $sql .= " WHERE id=" . $database->escape_string($this->id);
+
+
+        // $sql = "UPDATE ".self::$db_table." SET nickname ='".$database->escape_string($this->nickname)."', pass='".$database->escape_string($this->pass)."', first_name='".$database->escape_string($this->first_name)."', last_name='".$database->escape_string($this->last_name)."' WHERE id=".$database->escape_string($this->id)."";
 
         $database->query($sql);
 
@@ -144,7 +199,7 @@ class User {
 
         global $database;
 
-        $sql = "DELETE FROM users WHERE id =".$database->escape_string($this->id)." LIMIT 1;";
+        $sql = "DELETE FROM ".self::$db_table." WHERE id =".$database->escape_string($this->id)." LIMIT 1;";
 
         $database->query($sql);
 
