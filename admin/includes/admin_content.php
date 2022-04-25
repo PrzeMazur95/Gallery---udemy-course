c6e71e18186699eea449b2d7f0340a7e1144a8d9
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-25 10:09:06 +0200

    Add coments count view

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -64,7 +64,7 @@
                                         <i class="fa fa-user fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class="huge">8
+                                        <div class="huge"><?php echo User::count_all(); ?>
 
                                         </div>
 
