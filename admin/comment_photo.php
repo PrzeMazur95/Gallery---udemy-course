775d984d427f4a2f1dddba313ce4190f791d4e90
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-25 08:02:18 +0200

    Finish specific photo comment page

--- admin/comment_photo.php
+++ admin/comment_photo.php
@@ -12,7 +12,13 @@ if(!$session->is_signed_in()){
 if(empty($_GET['id'])){
 
     redirect('photos.php');
+
 }
+
+
+$comments = Comment::find_the_comments($_GET['id']);
+
+
 
 ?>
 
@@ -41,7 +47,7 @@ if(empty($_GET['id'])){
                 <h1 class="page-header">
                     comments
                 </h1>
-                <a href="add_comment.php" class="btn btn-primary">Add comment</a>
+                <!-- <a href="add_comment.php" class="btn btn-primary">Add comment</a> -->
             <div class="col-md-12">
 
                 <table class="table table-hover">
